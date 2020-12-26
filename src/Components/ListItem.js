0a87f771ff09201475fc6ff0b8968c63007b20a0
DESKTOP-UNVH3TB\Sahar <imsaharhayon@gmail.com>
2020-12-26 02:56:52 +0200

    Styled ADD and REMOVE icons.

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -29,7 +29,7 @@ function ListItem(props) {
             </div>
             <div className="card-body row p-0 text-wrap">
                 <div className="card-title text-decoration-underline font-weight-bold col-5 h5 m-0 user-select-none align-self-center" style={{fontSize: props.options.titlesSize}}>{props.title}</div>
-                <div className="col-1 mr-2" style={{transform: "translateY(40%)"}}><FontAwesomeIcon onClick={() => itemAdd(props.id, props.list, props.addItemToList, props.createItemInList)} icon={faPlusCircle}/></div>
+                <div className="col-1 mr-2" style={{transform: "translateY(40%)"}}><FontAwesomeIcon size="2x" onClick={() => itemAdd(props.id, props.list, props.addItemToList, props.createItemInList)} icon={faPlusCircle}/></div>
                 <div className="text-center h5 font-italic border-left border-right m-0 align-self-center">
                     {props.list[props.id] ? [
                         <button className="btn rounded-circle shadow-lg font-italic mx-2" style={{backgroundColor: "rgb(0, 0, 255, 0.5)", fontSize: "1.1rem"}} onClick={() => window.confirm("לאפס את המוצר?") && props.resetItemFromList(props.id)}><FontAwesomeIcon icon={faTimesCircle}/><br/>{props.list[props.id]}</button>,
@@ -37,7 +37,7 @@ function ListItem(props) {
                     :
                     <div className="rounded-circle shadow-lg col-1 font-italic mx-2" style={{fontSize: "1.1rem"}}>0</div>}
                 </div>
-                <div className="col-1 ml-2" style={{transform: "translateY(40%)"}}><FontAwesomeIcon className={`${!props.list[props.id] ? 'd-none' : ''}`} onClick={() => itemRemove(props.id, props.list, props.removeItemFromList)} icon={faMinusCircle}/></div>
+                <div className="col-1 ml-2" style={{transform: "translateY(40%)"}}><FontAwesomeIcon size="2x" className={`${!props.list[props.id] ? 'd-none' : ''}`} onClick={() => itemRemove(props.id, props.list, props.removeItemFromList)} icon={faMinusCircle}/></div>
                 <div className="card-title h-100 text-muted m-0 user-select-none align-self-center position-absolute" style={{right: '10px'}}>{props.category}</div>
             </div>
         </div>
